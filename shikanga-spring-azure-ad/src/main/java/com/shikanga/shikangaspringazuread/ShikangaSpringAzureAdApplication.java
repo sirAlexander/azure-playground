493c7436832dc50396f784d76daa1253befe5e0e
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-02-13 14:49:55 +0000

    initial commit of spring-azure-ad project

---
+++ shikanga-spring-azure-ad/src/main/java/com/shikanga/shikangaspringazuread/ShikangaSpringAzureAdApplication.java
@@ -0,0 +1,13 @@
+package com.shikanga.shikangaspringazuread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShikangaSpringAzureAdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShikangaSpringAzureAdApplication.class, args);
+    }
+
+}
---
+++ shikanga-spring-azure-ad/src/test/java/com/shikanga/shikangaspringazuread/ShikangaSpringAzureAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.shikangaspringazuread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShikangaSpringAzureAdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
